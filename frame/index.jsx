fba2ccb6329c62ce481f11c5ac6eea05e98909ae
yeonhwa <yeonhwa6717@gmail.com>
2019-10-16 21:22:08 +0900

    setting webpack config -- 웹팩설정 후 페이지 랜더링 해보기

---
+++ frame/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const Index = () => {
+  return(
+    <div>하이</div>
+  )
+}
+ReactDOM.render(<Index />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+process.env.NODE_ENV = ~process.argv[1].indexOf('webpack-dev-server.js') ? 'development' : 'production'
+
+module.exports = {
+  entry:{
+    app: './frame/index.jsx'
+  },
+  output: {
+    filename:'app.js',
+    path: path.resolve(__dirname,'dist')
+  },
+  resolve: {
+    extensions:['.js', '.jsx', '.png'] // 확장자 생략
+  },
+  devServer: {
+    hot: true,
+    overlay: true,
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 8000,
+  },// 임시서버, 라이브 리로딩 설정
+  module: {
+    rules:[
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react-app']
+          }
+        }
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
