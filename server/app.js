c12bffe8dcd4d6b8fe312b3fc8e53ace080cb2d3
yeonhwa <yeonhwa6717@gmail.com>
2019-11-11 20:51:35 +0900

    Lint and fix server code
    Install ESLint package.
    Fix server code.
    
    ESLint: https://eslint.org

--- server/app.js
+++ server/app.js
@@ -2,16 +2,15 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.on('connection', (socket) => { 
-  socket.on('test', data => {
-    console.log('data', data)
+const { log: print } = console;
 
-    io.emit('registered' , 'ㅇㄷㅁㅁㅈㅇㅅㅎ')
-  })
-  console.log('socket connected')
+io.on('connection', (socket) => {
+  socket.on('test', (data) => {
+    print('data', data);
 
+    io.emit('registered', 'ㅇㄷㅁㅁㅈㅇㅅㅎ');
+  });
+  print('socket connected');
 });
 
-server.listen(3000, () => {
-  return console.log('running 3000')
-});
+server.listen(3000, () => print('running 3000'));
